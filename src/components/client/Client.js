3fae30f177f5cdb9a04845e4d31d3f2ed38ab844
prosanto-deb <prosantodeb408@gmail.com>
2022-12-15 02:29:15 +0600

    slide update

--- src/components/client/Client.js
+++ src/components/client/Client.js
@@ -1,7 +1,34 @@
 import React from 'react'
 import "./client.css"
-
-export const Client = () => {
+import Slider from "react-slick";
+ const Client = () => {
+    function SampleNextArrow(props) {
+		const { onClick } = props;
+		return (
+		  <div onClick={onClick} className="i-left">
+			<i class="fa-solid fa-chevron-left"></i>
+		  </div>
+		  
+		);
+	  }
+	  
+	  function SamplePrevArrow(props) {
+		const { onClick } = props;
+		return (
+		  <div onClick={onClick} className="i-right">
+			<i class="fa-solid fa-chevron-right"></i>
+		  </div>
+		);
+	  }
+	  
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		 nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
+      };
   return (
     <div className="client">
         <div className="container">
@@ -13,24 +40,49 @@ export const Client = () => {
                     </p>
                 </div>
                 <div className="client-right">
-                        <h5>
-                            "The best agency we’ve worked with so far. They understand our product and are able to add new features with a great focus."
-                        </h5>
-                        <div className="president">
-                            <div className="president-left">
-                                <img src="images/president.png" alt=""/>
-                                <div className="prisedent-name">
-                                    <h5>Jenny Wilson</h5>
-                                    <h6>Vice President</h6>
-                                    
-                                </div>
-                            </div>
-                            <div className="arrow-icon">
-                             <i className="fa-solid i-left fa-chevron-left"></i>
-                             <i className="fa-solid i-right fa-chevron-right"></i>
-                         </div>
-                    </div>  
-                </div>
+					<Slider {...settings}>
+                        <div>
+							<h5>
+								"The best agency we’ve worked with so far. They understand our product and are able to add new features with a great focus."
+							</h5>
+							
+							<div className="president">
+								<div className="president-left">
+									<img src="images/president.png" alt=""/>
+									<div className="prisedent-name">
+										<h5>Jenny Wilson</h5>
+										<h6>Vice President</h6>
+										
+									</div>
+								</div>
+								<div className="arrow-icon">
+									{/* <i className="fa-solid i-left fa-chevron-left"></i>
+									<i className="fa-solid i-right fa-chevron-right"></i> */}
+								</div>
+							</div>
+						</div>
+					<div>
+							<h5>
+								"The best agency we’ve worked with so far. They understand our product and are able to add new features with a great focus."
+							</h5>
+							
+							<div className="president">
+								<div className="president-left">
+									<img src="images/president.png" alt=""/>
+									<div className="prisedent-name">
+										<h5>Jenny Wilson</h5>
+										<h6>Vice President</h6>
+										
+									</div>
+								</div>
+								<div className="arrow-icon">
+									{/* <i className="fa-solid i-left fa-chevron-left"></i>
+									<i className="fa-solid i-right fa-chevron-right"></i> */}
+								</div>
+							</div>
+						</div>
+                    </Slider>
+                </div>			
             </div>
         </div>
     </div>
